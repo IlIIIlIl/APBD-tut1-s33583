ebd19aa6006831a5050c01b4c9cd723836e07f4c
IlIIIlIl <s33583@pjwstk.edu.pl>
2026-03-10 13:31:10 +0100

    Added a small java program

---
+++ file.java
@@ -0,0 +1,20 @@
+﻿import java.util.Random;
+import java.util.Scanner;
+
+public class file {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("How many random numbers do you want? ");
+        int count = scanner.nextInt();
+
+        System.out.println("Here are your numbers:");
+        for (int i = 0; i < count; i++) {
+            int number = random.nextInt(101);
+            System.out.println(number);
+        }
+
+        scanner.close();
+    }
+}
