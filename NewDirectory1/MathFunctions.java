c4a9f0e0b4b842c5eb7ad67ae2c0d0455f300b87
IlIIIlIl <s33583@pjwstk.edu.pl>
2026-03-16 23:25:46 +0100

    Completed the function CalculateMin

--- NewDirectory1/MathFunctions.java
+++ NewDirectory1/MathFunctions.java
@@ -13,7 +13,14 @@ public class MathFunctions {
 
 
     public int CalculateMin(int[] values) {
-       
+       int min = values[0];
+
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] < min) {
+                min = values[i];
+            }
+        }
+        return min;
     }
 
 }
